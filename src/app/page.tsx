20f71f387d4acab8f8a8e0b6d1bfa4d9af0e57c3
= <=>
2024-04-17 12:55:56 +0700

    Done Praktikum 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Tombol_1 } from "./component/button";
+import { Tombol_2 } from "./component/button";
 
 export default function Home(){
   return(
@@ -7,6 +8,8 @@ export default function Home(){
       <div className="container mx-auto">
         <h2>Kuis Kota</h2>
         <Tombol_1 />
+        <hr></hr>
+        <Tombol_2 isiPesan="Ini Pesanku" namaTombol="Pesan" />
       </div>
     </>
   );
